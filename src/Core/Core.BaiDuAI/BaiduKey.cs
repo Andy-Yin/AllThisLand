4e2a6c4730967e614d0f694e7d970b909cda3053
yincun <ustcyc@gmail.com>
2020-07-17 17:06:41 +0800

    1

---
+++ src/Core/Core.BaiDuAI/BaiduKey.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Core.BaiDuAI
+{
+    public class BaiduKey
+    {
+        // 设置APPID/AK/SK
+        public static string APP_ID = "21441048";
+        public static string API_KEY = "SDu4RRmZdgGH7ntwZL8iGDug";
+        public static string SECRET_KEY = "vCxhg2zmn08PCF6FQDMvlqKjvK6Zrwmw";
+    }
+}
--- src/Service/LhsApi/Controllers/IndexController.cs
+++ src/Service/LhsApi/Controllers/IndexController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.BaiDuAI;
 using Core.Data;
 using Lhs.Common.Const;
 using Lhs.Common.Enum;
@@ -37,8 +38,8 @@ namespace LhsAPI.Controllers
         /// <summary>
         /// 首页获取项目统计-后台
         /// </summary>
-        [HttpGet("getProjectStatistics")]
-        public async Task<ResponseMessage> GetProjectStatistics([FromQuery]ReqAuth req)
+        [HttpGet("testbaidu")]
+        public async Task<ResponseMessage> TestBaidu([FromQuery] ReqAuth req)
         {
             var response = new RespProjectStatistics();
             var result = new ResponseMessage
@@ -48,57 +49,12 @@ namespace LhsAPI.Controllers
                 Data = response
             };
             var dataList = await _projectRepository.GetAllProject();
-
-            //本月统计
-            response.AddCount = dataList.Count(n => n.CreateTime.Year == DateTime.Now.Year && n.CreateTime.Month == DateTime.Now.Month).ToString();
-            response.PlanStartCount = dataList.Count(n => n.PlanStartDate.Year == DateTime.Now.Year && n.PlanStartDate.Month == DateTime.Now.Month).ToString();
-            response.PlanEndCount = dataList.Count(n => n.PlanEndDate?.Year == DateTime.Now.Year && n.PlanEndDate?.Month == DateTime.Now.Month).ToString();
-            //todo：这两个数量暂时不知道怎么获取
-            response.AddAmount = "0";
-            response.SettlementCount = "0";
-            //项目统计
-            response.TotalCount = dataList.Count.ToString();
-            response.ToBeStartCount = dataList.Count(n => n.Status == (int)ProjectEnum.ProjectStatus.WaitStart).ToString();
-            response.PreparationCount = dataList.Count(n => n.Status == (int)ProjectEnum.ProjectStatus.Prepare).ToString();
-            response.ConstructingCount = dataList.Count(n => n.Status == (int)ProjectEnum.ProjectStatus.Building).ToString();
-            response.CompletedCount = dataList.Count(n => n.Status == (int)ProjectEnum.ProjectStatus.Complete).ToString();
-            response.StoppedCount = dataList.Count(n => n.Status == (int)ProjectEnum.ProjectStatus.Stop).ToString();
-            //项目进度
-            response.ApprovingPackageCount = dataList.Count(n => n.FollowId > (int)ProjectConst.ProjectFlowStartId).ToString();
-            response.AddPackageCount = dataList.Count(n => n.FollowId == (int)ProjectConst.ProjectFlowStartId).ToString();
+            var client = new Baidu.Aip.Ocr.Ocr(BaiduKey.API_KEY, BaiduKey.SECRET_KEY);
+            client.Timeout = 60000;  // 修改超时时间
 
             result.ErrMsg = CommonMessage.OperateSuccess;
             result.ErrCode = MessageResultCode.Success;
-            result.Data = response;
-            return result;
-        }
-
-
-        /// <summary>
-        /// 首页获取消息列表-后台
-        /// </summary>
-        [HttpGet("getNewsList")]
-        public async Task<ResponseMessage> GetNewsList([FromQuery]ReqAuth req)
-        {
-            var response = new RespNewsList();
-            var result = new ResponseMessage
-            {
-                ErrMsg = CommonMessage.OperateFailed,
-                ErrCode = MessageResultCode.Error,
-                Data = response
-            };
-            if (req.OperatorId <= 0)
-            {
-                return result;
-            }
-
-            var dataList = await _userRepository.GetUserApproveMessageList(req.OperatorId);
-
-            response.DataList = dataList.Select(n => new RespUserApproveMessage(n)).ToList();
-            result.Data = response;
-
-            result.ErrMsg = CommonMessage.OperateSuccess;
-            result.ErrCode = MessageResultCode.Success;
+            result.Data = client.AppId;
             return result;
         }
     }
